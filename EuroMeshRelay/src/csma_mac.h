2e11b7ce5b5021fa26893633eefd7078d8ddd2f3
Tomás Martins <tomasmartins@me.com>
2026-02-09 23:04:14 +0000

    Validate packet types and tighten parsing

--- EuroMeshRelay/src/csma_mac.h
+++ EuroMeshRelay/src/csma_mac.h
@@ -18,6 +18,7 @@ typedef struct {
 
 void csma_mac_init(csma_mac_t *mac, sx1276_t *radio, uint8_t rssi_threshold, uint32_t backoff_min_ms, uint32_t backoff_max_ms, uint8_t max_attempts);
 bool csma_mac_is_channel_clear(csma_mac_t *mac);
+/* payload[0] must contain the EMESH packet type. */
 HAL_StatusTypeDef csma_mac_send(csma_mac_t *mac, const sx1276_packet_header_t *header, const uint8_t *payload, uint8_t payload_length);
 HAL_StatusTypeDef csma_mac_send_tdma(csma_mac_t *mac, const sx1276_packet_header_t *header, const uint8_t *payload, uint8_t payload_length, uint32_t frame_start_ms, uint32_t slot_length_ms, uint8_t slot_index);
 
